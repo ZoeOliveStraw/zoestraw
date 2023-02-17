752dd1b3c67bf09f3e1afb6871a289813be62cd5
Zoe Straw <zoe.olive.straw@gmail.com>
2023-02-17 18:07:02 -0500

    Updating website to be more professional and complete

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,63 +1,31 @@
-//IIFE -- Immediately invoked function express
-//AKA anonymous self-executing function
 (function()
 {
-    function DisplayHome()
+    function DisplayNavBar()
     {
-        let AboutUsButton = document.getElementById("AboutUsButton");
-        AboutUsButton.addEventListener("click", function()
-        {
-            location.href = "about.html";
-        });
-
-        //Step 1 - get a reference to an entry point(s) (insertion / deletion point)
-        let MainContent = document.getElementsByTagName("main")[0];
-        let DocumentBody = document.body;
-        
-        //Step 2 - Create an HTML element in memory
-        let MainParagraph = document.createElement("p");
-        let Article = document.createElement("article");
-        let ArticleParagraph = `<p id="ArticleParagraph" class="mt-3">
-        This is the Article Paragraph</p>`;
-
-        //Step 3 - Configure new element
-        MainParagraph.setAttribute("id","MainParagraph");
-        MainParagraph.setAttribute("class","mt-3");
-        let FirstString = "Adding some sickass shit ";
-        let SecondString = `${FirstString} to the main paragraph.`;
-        MainParagraph.textContent = SecondString;
-        Article.setAttribute("class","container");
+        console.log("Hello, World!");
+        $('#header').load("navbar.html");
+    }
 
     
-
-        //Step 4 - Perform insertion / deletion
-
-        //Example of Insert After / Append
-        MainContent.appendChild(MainParagraph);
-        Article.innerHTML = ArticleParagraph;
-        DocumentBody.appendChild(Article);
-
-        //Example of Insert Before
-        //MainContent.before(MainParagraph);
-
-        //Example of deletion
-
-        //AboutUsButton.remove();
-
-        //ES6 HTML5 => Template Strings => "Super Strings"
-        
+    function AddBackgroundImage()
+    {
+        console.log("Adding background image!");
+        $('body').css({'background-image':'url("../art/background.jpg")',
+                        'background-size' : 'cover'});
+        $('body').addClass("has-bg-img");
     }
+
+    function StyleCards()
+    {
+        $("[id='cards']").css('background-color', 'rgb(219, 224, 255,0.8)')
+    }
+
     //named function
     function Start()
     {
-        console.log("Hello, World!");
-
-        switch(document.title)
-        {
-            case "Home Page":
-                DisplayHome();
-                break;
-        }
+        DisplayNavBar();
+        AddBackgroundImage();
+        StyleCards();
     }
 
     window.addEventListener("load", Start());
